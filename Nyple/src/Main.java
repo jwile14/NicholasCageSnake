6a6973f3125c1438c9088b271e73d13b9a9cc77a
jwile14 <wilejd@rose-hulman.edu>
2014-11-22 17:38:39 -0500

    Bitches

--- Nyple/src/Main.java
+++ Nyple/src/Main.java
@@ -21,25 +21,9 @@ public class Main {
 		frame.setTitle("Nyple - 2.0");	
 		frame.setSize(new Dimension(600,600));
 		
-		//Comment hi and bye
-		System.out.println("Justin".length());
-		int a = 0;
-		char character = 54;
-		String j = "Blah";
-		String q = "Whoo";
-		
-		if(j.charAt(0) == q.charAt(0));
-		
-		if(j.substring(0,1).equals(q.substring(0,1)));
-		
 		//JPanel panel = new JPanel();
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-	}
-	
-	
-	public static int compute(String stg) {
-		return stg.length();
 	}
 }
