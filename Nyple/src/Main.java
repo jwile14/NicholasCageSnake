7be578b4af96b91106270cfcbe7f2d2eea2aec0d
jwile14 <wilejd@rose-hulman.edu>
2014-11-22 14:48:14 -0500

    Initial test

---
+++ Nyple/src/Main.java
@@ -0,0 +1,26 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author wilejd.
+ *         Created Nov 22, 2014.
+ */
+public class Main {
+	/**
+	 * Main method.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		
+		frame.setTitle("Nyple");	
+		frame.setSize(new Dimension(600,600));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
