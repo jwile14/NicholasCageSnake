ee0aa92bce519d52c7d5458b0b6c5de12e11a217
unknown <brandsm@BRANDSM-W540.rose-hulman.edu>
2014-11-23 00:06:00 -0500

    dsc

--- Nyple/src/GameKeyListener.java
+++ Nyple/src/GameKeyListener.java
@@ -15,17 +15,16 @@ public class GameKeyListener implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		if (arg0.getKeyCode() == KeyEvent.VK_UP){
+		if (arg0.getKeyCode() == KeyEvent.VK_UP && !(nick.getDirection() == 270)){
 			nick.setDirection(90);
-			
 			lc.repaint();
-		}else if (arg0.getKeyCode() == KeyEvent.VK_DOWN){
+		}else if (arg0.getKeyCode() == KeyEvent.VK_DOWN && !(nick.getDirection() == 90)){
 			nick.setDirection(270);
 			lc.repaint();
-		}else if (arg0.getKeyCode() == KeyEvent.VK_LEFT){
+		}else if (arg0.getKeyCode() == KeyEvent.VK_LEFT && !(nick.getDirection() == 0)){
 			nick.setDirection(180);
 			lc.repaint();
-		}else if (arg0.getKeyCode() == KeyEvent.VK_RIGHT){
+		}else if (arg0.getKeyCode() == KeyEvent.VK_RIGHT && !(nick.getDirection() == 180)){
 			nick.setDirection(0);
 			lc.repaint();
 		}
