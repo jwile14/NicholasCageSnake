d8c5bcbd1ffdb5c49b9841ba90dccd1a632f8a3e
jwile14 <wilejd@rose-hulman.edu>
2014-11-22 22:46:12 -0500

    Blah

--- Nyple/src/Cage.java
+++ Nyple/src/Cage.java
@@ -1,3 +1,4 @@
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -5,11 +6,13 @@ import javax.imageio.ImageIO;
 
 
 public class Cage extends Sprite{
-
+	private BufferedImage img2;
+	
 	public Cage(int xPos, int yPos) {
 		super(xPos, yPos);
 		try {
-			this.setImage(ImageIO.read(new File("nic cage close mouth.jpg")));
+			this.setImage(ImageIO.read(new File("nic cage close mouth.png")));
+			this.img2 = ImageIO.read(new File("nic cage open mouth.png"));
 		} catch (IOException e) {
 			throw new RuntimeException("No image file for Nobbin");
 		}
--- Nyple/src/Level.java
+++ Nyple/src/Level.java
@@ -27,8 +27,8 @@ public class Level {
 		
 		for(Sprite s : this.sprites) {
 			System.out.println("SWAG");
-			g2d.drawImage(s.getImage(), s.getX() * this.CELL_WIDTH/2, s.getY()
-					* this.CELL_WIDTH/2, this.CELL_WIDTH/2, this.CELL_WIDTH/2,
+			g2d.drawImage(s.getImage(), s.getX() * this.CELL_WIDTH, s.getY()
+					* this.CELL_WIDTH, this.CELL_WIDTH, this.CELL_WIDTH,
 					null);
 		}
 	}
